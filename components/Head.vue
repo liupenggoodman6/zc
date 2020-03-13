a4637df897d1aa70b8f06396ccb1cc7715eef987
1948966136@qq.com <1948966136@qq.com>
2020-03-13 13:47:00 +0800

    first commit

---
+++ components/Head.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="workspace-top">toubuxinxi</div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.headerbar{
+  width:100%;
+  padding-right: 23px;
+  padding-left: 22px;
+
+}
+</style>
